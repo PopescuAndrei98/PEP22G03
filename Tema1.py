f98cb83c24ee55b48789c22e4e6d36bafbe4c2d0
popes <popescu_andrei2601@yahoo.com>
2022-06-06 19:34:38 +0000

    tema1

---
+++ Tema1.py
@@ -0,0 +1,44 @@
+#text="Passme1n"
+#parola=input()
+#print(parola==text)
+
+
+#nume1=input()
+#nume2=input()
+
+#lungime1=len(nume1)-1
+#lungime2=len(nume2)-1
+
+#print("lungime1=",lungime1)
+#print("Nume egale?", nume1==nume2)
+#print("Nume1 este mai lung decat nume2",lungime1>lungime2)
+#print("Initiala numelui 1 este:", nume1[0])
+#print("Numele inversat:",nume1[::-1])
+
+
+
+##nume1=input()
+##nume2=input()
+##numar=input()
+
+##lungime1=len(nume1)
+##lungime2=len(nume2)
+
+##print("lungime1=",lungime1)
+##print("Nume egale?", nume1==nume2)
+##print("Nume1 este mai lung decat nume2",lungime1>lungime2)
+##print("Initiala numelui 1 este:", nume1[0])
+##print("Numele inversat:",nume1[::-1])
+
+##print("Introduceti numarul:"+str(numar*nume1))
+
+
+
+cuvantul="Ananas"
+
+print(cuvantul[0],"\n",cuvantul[1],"\n",cuvantul[2],"\n",cuvantul[3],"\n",cuvantul[4],"\n",cuvantul[5],"\n")
+print(cuvantul[0:3])
+print(cuvantul[3:6])
+print(cuvantul[0:2],":",cuvantul[2:5],":",cuvantul[5])
+print(cuvantul[0:3],"_",cuvantul[3:5],"_",cuvantul[5])
+print(cuvantul[1:3]*8)
